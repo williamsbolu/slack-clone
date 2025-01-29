1b016d3e6ba0a5a01702d3b14fce6b3660ef6703
williams bolu <williams.bolu99@gmail.com>
2025-01-29 15:27:27 +0100

    fix hompage loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
-import { UserButton } from "@/features/auth/components/user-button";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -26,8 +26,8 @@ export default function Home() {
   }, [workspageId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
